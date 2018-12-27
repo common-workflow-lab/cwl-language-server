8267495bcf8d24fc782b62b470dcaea5a0ba1c52
Tomoya Tanjo <ttanjo@gmail.com>
2018-12-27 16:39:17 +0900

    Fix stdin and stdout

--- cwl_language_server/main.py
+++ cwl_language_server/main.py
@@ -6,7 +6,9 @@ import pylspclient
 import callbacks
 
 def main():
-    json_rpc_endpoint = pylspclient.JsonRpcEndpoint(sys.stdin, sys.stdout)
+    stdin = sys.stdout
+    stdout = sys.stdin
+    json_rpc_endpoint = pylspclient.JsonRpcEndpoint(stdin, stdout)
     lsp_endpoint = pylspclient.LspEndpoint(json_rpc_endpoint, default_callback=print,
                                            callbacks={ 'completion': callbacks.completion })
     lsp_endpoint.run()
